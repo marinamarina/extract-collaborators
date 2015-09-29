cd40d68ccdcb0bdea5018b2c1b281e3ca367dbb3
Marina Shchukina <shchukina.marina@gmail.com>
2015-09-29 11:17:31 +0100

    added anonymous function

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-var jsonfile = require('jsonfile'),
-	util = require('util'),
+(function () { 
+    var jsonfile = require('jsonfile'),
+    util = require('util'),
 	file = 'box_collaborations.json',
 	emailListCreated = [],
 	emailListAccessible = [],
 	emailListCreatedUnique = [],
 	emailListAccessibleUnique = [];
 
-Array.prototype.arrayUnique = function() {
-    return this.reduce(function(p, c) {
-        if (p.indexOf(c) < 0) p.push(c);
-        return p;
-    }, []);
-};
+    Array.prototype.arrayUnique = function() {
+        return this.reduce(function(p, c) {
+            if (p.indexOf(c) < 0) p.push(c);
+                return p;
+            }, []);
+    };
  
-jsonfile.readFile(file, function(err, obj) {
+    jsonfile.readFile(file, function(err, obj) {
   
-  for(var i=0; i < obj.entries.length; i++) {
-  	if (obj.entries[i].created_by) {
-  		var emailCreated = obj.entries[i].created_by.login;
-  		emailListCreated.push(emailCreated);
-  	}
+    for(var i=0; i < obj.entries.length; i++) {
+  	     if (obj.entries[i].created_by) {
+  		    var emailCreated = obj.entries[i].created_by.login;
+  		    emailListCreated.push(emailCreated);
+  	     }
   	
-  	if (obj.entries[i].accessible_by) {
-  		var emailAccessible = obj.entries[i].accessible_by.login;
-  		emailListAccessible.push(emailAccessible);
-  	}
+  	     if (obj.entries[i].accessible_by) {
+  		    var emailAccessible = obj.entries[i].accessible_by.login;
+  		    emailListAccessible.push(emailAccessible);
+  	     }
   				
-  }
-emailListCreatedUnique = emailListCreated.arrayUnique();
+    }
+    emailListCreatedUnique = emailListCreated.arrayUnique();
 
- console.log("\r\n List of Created By:");
+    console.log("\r\n List of Created By:");
 
- for (var i=0; i< emailListCreatedUnique.length; i++) {
- 	console.log(emailListCreatedUnique[i]);
- }
+    for (var i=0; i< emailListCreatedUnique.length; i++) {
+ 	      console.log(emailListCreatedUnique[i]);
+    }
 
-console.log("\r\n List of Accessible By:");
+    console.log("\r\n List of Accessible By:");
 
- for (var i=0; i< emailListAccessible.length; i++) {
- 	console.log(emailListAccessible.arrayUnique()[i]);
- }
-
+    for (var i=0; i< emailListAccessible.length; i++) {
+ 	      console.log(emailListAccessible.arrayUnique()[i]);
+    }
 });
+} ());
